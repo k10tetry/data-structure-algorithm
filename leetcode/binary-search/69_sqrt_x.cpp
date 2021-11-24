c4aab558772a6c570416476b0f52553ab062c1e3
k10tetry <k10tetry@gmail.com>
2021-11-25 00:41:49 +0530

    update the sqrt solution using better approach

--- leetcode/binary-search/69_sqrt_x.cpp
+++ leetcode/binary-search/69_sqrt_x.cpp
@@ -4,8 +4,26 @@ using namespace std;
 /* https://leetcode.com/problems/sqrtx/ */
 
 /*
-Solved using binary-search algorithm, 6ms and 25 faster solution.
+Solved using binary-search algorithm, 6ms and 25 faster solution. Instead of using long long mid variable evaulate the expression as mid <= x/mid
 */
+
+// Another efficient solution
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 1, r = x, mid, ans = 0;
+        while (l <= r){
+            mid = l + (r - l) / 2;
+            if (mid <= x/mid){
+                l = mid + 1;
+                ans = mid;
+            } else{
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
 
 class Solution{
 public:
