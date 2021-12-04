8b5e2d1f3762617440519833a357b0db4864ab05
k10tetry <k10tetry@gmail.com>
2021-12-04 23:58:43 +0530

    solved one codechef long challenge problem

---
+++ codechef/listlist_list_of_lists.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define space ' '
+#define rep(i,s,e) for (int i = s; i < e; ++i)
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+
+		int n, num, mx = 0;
+		unordered_map<int,int> m;
+
+		cin >> n;
+
+		rep(i,0,n){
+			cin >> num;
+			m[num]++;
+		}
+
+		for (pair<int,int> p : m){
+			mx = max(mx,p.second);
+		}
+
+		if(mx == n){
+			cout << 0 << endl;
+		}else if(mx > 1){
+			cout << n - mx + 1 << endl;
+		}else{
+			cout << -1 << endl;
+		}
+
+	}
+
+	return 0;
+}
